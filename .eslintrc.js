e0e2fc5daff51daf2eaad44eb4dc76be8ee25d6f
Max Zinchenko <zzeenj@gmail.com>
2021-04-14 20:06:46 +0300

    add types for the Input and Form props

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: ['./tsconfig.json'],
     ecmaFeatures: {
       jsx: true
     }
--- src/hooks/use-form.tsx
+++ src/hooks/use-form.tsx
@@ -1,13 +1,26 @@
-import React, { useState, useCallback, useMemo, memo, FormEvent, ChangeEvent, InputHTMLAttributes, FormHTMLAttributes } from 'react';
+import React, { useState, useCallback, useMemo, memo, FormEvent, ChangeEvent, InputHTMLAttributes, FormHTMLAttributes, FC } from 'react';
 
 import { State, Options, StateValue } from '../types';
 import { validateValue } from '../services/validation';
 
 type Values<T> = Record<keyof T, string | null>;
-type SubmitCallback<T> = (event: FormEvent<HTMLFormElement>, data: Values<T>) => void;
 
-export const useForm = <S extends State>(initialState: S, options: Options<S>, submitCallback: SubmitCallback<S>) => {
+interface AwesomeInputProps<T> extends InputHTMLAttributes<HTMLElement> {
+  name: keyof T & string;
+}
+
+interface AwesomeFormProps<T> extends FormHTMLAttributes<HTMLElement> {
+  handleSubmit: (event: FormEvent<HTMLFormElement>, data: Values<T>) => void;
+}
+
+export const useForm = <S extends State>(initialState: S, validationOptions: Options<S>) => {
   const [state, setState] = useState(initialState);
+
+  const disabled = useMemo((): boolean => {
+    const errors = getValues(state, 'error');
+
+    return !!Object.values(errors).filter(error => !!error).length;
+  }, [state]);
 
   const getValues = useCallback((target: S, key: keyof StateValue): Values<S> => {
     const values = {} as Values<S>;
@@ -19,32 +32,20 @@ export const useForm = <S extends State>(initialState: S, options: Options<S>, s
     return values;
   }, []);
 
-  const disabled = useMemo((): boolean => {
-    const errors = getValues(state, 'error');
-
-    return !!Object.values(errors).filter(error => !!error).length;
-  }, [state]);
-
   const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.currentTarget;
 
-    validateValue(options[name], value, error => {
+    validateValue(validationOptions[name], value, error => {
       setState((prevState) => ({ ...prevState, [name]: { value, error } }));
     });
   }, []);
 
-  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
-    if (disabled) return;
-
-    submitCallback(event, getValues(state, 'value'));
-  }, [disabled]);
-
-  const Input = memo<InputHTMLAttributes<{}>>(props => (
-    <input { ...props } onChange={ props.onChange || handleChange }/>
+  const Input = memo<AwesomeInputProps<S>>(props => (
+    <input { ...props } onChange={ handleChange }/>
   ));
 
-  const Form = memo<FormHTMLAttributes<{}>>(({ children, ...props }) => (
-    <form { ...props } onSubmit={ props.onSubmit || handleSubmit }>
+  const Form: FC<AwesomeFormProps<S>> = (({ children, ...props }) => (
+    <form { ...props } onSubmit={ event => props.handleSubmit(event, getValues(state, 'value')) }>
       { children }
     </form>
   ));
