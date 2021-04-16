4280e89cda111537ed094ca9e9bd24bbd7654ca1
Max Zinchenko <zzeenj@gmail.com>
2021-04-16 23:14:50 +0300

    fix reference error

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -8,12 +8,6 @@ type Values<T> = Record<keyof T, string | null>;
 export const useForm = <S extends State>(initialState: S, validationOptions: Options<S>) => {
   const [state, setState] = React.useState(initialState);
 
-  const disabled = React.useMemo((): boolean => {
-    const errors = getValues(state, 'error');
-
-    return !!Object.values(errors).filter(error => !!error).length;
-  }, [state]);
-
   const getValues = React.useCallback((target: S, key: keyof StateValue): Values<S> => {
     const values = {} as Values<S>;
 
@@ -31,6 +25,12 @@ export const useForm = <S extends State>(initialState: S, validationOptions: Opt
       setState((prevState) => ({ ...prevState, [name]: { value, error } }));
     });
   }, []);
+
+  const disabled = React.useMemo((): boolean => {
+    const errors = getValues(state, 'error');
+
+    return !!Object.values(errors).filter(error => !!error).length;
+  }, [state]);
 
   return {
     state,
